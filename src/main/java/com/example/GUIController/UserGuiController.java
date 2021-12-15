0621876b527b3ba4684e95bd46a942c3cce2a3cf
Cristea Codrin <codrincristea21@gmail.com>
2021-12-15 10:20:54 +0200

    Fixed Register

--- src/main/java/com/example/GUIController/UserGuiController.java
+++ src/main/java/com/example/GUIController/UserGuiController.java
@@ -12,10 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
@@ -144,7 +141,7 @@ public class UserGuiController  {
 
              Relationship rel = new Relationship(user.getUsername(), username, LocalDate.now(), "pending");
             cont.AddRequest(rel);
-            MessageAlert.showMessage(null,null,"Add", "cerere trimisa cu succes");
+            MessageAlert.showMessage(null, Alert.AlertType.INFORMATION,"Add", "cerere trimisa cu succes");
         }
         catch (Exception e)
         {
